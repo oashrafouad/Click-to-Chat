c5f02a88c25b9a0ad89ac50caaf9dbbfc1ff7c6b
Omar Ashraf <oashrafouad@gmail.com>
2024-12-04 22:56:29 +0200

    Initial Commit

---
+++ Click to Chat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Click to Chat
+//
+//  Created by Omar Ashraf on 04/12/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
