5ff991baa4b0fee62fbcd758445396d6f9879a2b
Omar Ashraf <oashrafouad@gmail.com>
2024-12-05 14:31:47 +0200

    added app logic

--- Click to Chat/ViewController.swift
+++ Click to Chat/ViewController.swift
@@ -1,19 +1,41 @@
-//
-//  ViewController.swift
-//  Click to Chat
-//
-//  Created by Omar Ashraf on 04/12/2024.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var phoneTextField: UITextField!
+    var formattedNumber: String = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        phoneTextField.delegate = self
     }
 
-
+    @IBAction func messageButtonPressed(_ sender: UIButton) {
+        guard let phoneNumber = phoneTextField.text, !phoneNumber.isEmpty else {
+            let alert = UIAlertController(title: "Error", message: "Please enter a phone number.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+            return
+        }
+        
+        formattedNumber = phoneNumber.replacingOccurrences(of: " ", with: "")
+        let whatsappURL = URL(string: "https://wa.me/\(formattedNumber)")!
+        
+        if UIApplication.shared.canOpenURL(whatsappURL) {
+            UIApplication.shared.open(whatsappURL, options: [:], completionHandler: nil)
+        } else {
+            let alert = UIAlertController(title: "Error", message: "WhatsApp is not installed on your device.", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alert, animated: true, completion: nil)
+        }
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        phoneTextField.endEditing(true)
+    }
+    
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        formattedNumber = (textField.text?.replacingOccurrences(of: " ", with: ""))!
+        textField.text = formattedNumber
+    }
 }
-
